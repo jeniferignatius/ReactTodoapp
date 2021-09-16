f32572aed8855af643ebe2d01f175ae595c5aa38
Jeniferignatius <jeni.igni@gmail.com>
2021-09-16 14:21:25 +0300

    Todoapp added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const[Todo, setTodo] = useState( {description:'', date: ''});
+  const[Todos, setTodos] = useState([]);
+
+  const inputChanged = (event) => {
+    setTodo({...Todo, [event.target.name]:event.target.value });
+  }
+ 
+  const Addtodo = () => {
+    setTodos([...Todos, Todo]);
+  }
+
+  const deleteTodo = (row)=>{
+    setTodos (Todos.filter((Todo, index) => index !== row))
+  }
+  return (
+    <div className="App">
+    
+     <h3>Todo App</h3><br />
+<input placeholder = "description" name= "description" value={Todo.description} onChange={inputChanged}/>
+<input placeholder = "date" name= "date" value={Todo.date} onChange={inputChanged}/>
+<button onClick={Addtodo}>Add</button><br/>
+<table>
+  <tbody>
+    {
+      Todos.map((Todo, index)=>
+      <tr key ={index}>
+       <td>
+        {Todo.description}
+       </td>
+       <td>
+        {Todo.date}
+       </td>
+       <td>
+      <button onClick= {()=>deleteTodo(index)}>Delete</button>
+       </td>
+      </tr>
+      )
+    }
+  </tbody>
+</table>
+</div>
+);
 }
 
 export default App;
